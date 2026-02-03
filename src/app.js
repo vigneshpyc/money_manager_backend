645aa9ce12b6c4c9df01de40f2dfba1472b49c1c
vigneshpyc <jeevaviki1609@gmail.com>
2026-02-03 23:02:42 +0530

    update_cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,19 @@ const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
+
+app.use(express.json());
+
+
 app.use(cors());
 app.use(express.json());
 
